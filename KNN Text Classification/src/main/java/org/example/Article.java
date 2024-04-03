4ea41ef18b3cc499bfb25398b9e4219cc4b91c6f
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-03 20:16:06 +0200

    add articles loading

---
+++ KNN Text Classification/src/main/java/org/example/Article.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Article {
+    private String title;
+    private String country;
+    private String body;
+
+    public Article(String title, String country, String body) {
+        this.title = title;
+        this.country = country;
+        this.body = body;
+    }
+}
