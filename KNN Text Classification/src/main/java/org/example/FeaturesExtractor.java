86de0246e9d9d6e0520b48244536e8cd69e63ab8
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-04 19:48:38 +0200

    add extractMostCommonBigLetterWordInFirstXPercent()

--- KNN Text Classification/src/main/java/org/example/FeaturesExtractor.java
+++ KNN Text Classification/src/main/java/org/example/FeaturesExtractor.java
@@ -2,7 +2,9 @@ package org.example;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,15 +14,34 @@ public class FeaturesExtractor {
         return null;
     }
 
-    public String extractFirstBigLettersSeries(String text) {
-        Pattern pattern = Pattern.compile("\\b[A-ZŻŹĆĄŚĘŁÓ][a-zżźćńółęąś]*\\s[A-ZŻŹĆĄŚĘŁÓ][a-zżźćńółęąś]*\\b");
-        Matcher matcher = pattern.matcher(text);
+    public String extractMostCommonBigLetterWordInFirstXPercent(String text, double percentage) {
+        String[] words = text.trim().split("\\s+");
+        int numberOfWords = getNumberOfWordsInText(text);
+        int numberOfWordsToConsider = (int)(numberOfWords * percentage);
 
-        if (matcher.find()) {
-            return matcher.group();
-        } else {
-            return null;
+        int maxCount = 0;
+        String mostFrequentWord = null;
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+        for (int i = 0; i < numberOfWordsToConsider; i++) {
+            String word = words[i];
+            if (Character.isUpperCase(word.charAt(0))) {
+                wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+                if (wordCounts.size() == 1) {
+                    maxCount += 1;
+                    mostFrequentWord = word;
+                }
+            }
         }
+
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mostFrequentWord = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        return mostFrequentWord;
     }
 
     public double extractRelativeNumberOfUniqueWords(String text) {
@@ -48,6 +69,17 @@ public class FeaturesExtractor {
         }
 
         return (double)longWordCount / getNumberOfWordsInText(text);
+    }
+
+    public String extractFirstBigLettersSeries(String text) {
+        Pattern pattern = Pattern.compile("\\b[A-ZŻŹĆĄŚĘŁÓ][a-zżźćńółęąś]*\\s[A-ZŻŹĆĄŚĘŁÓ][a-zżźćńółęąś]*\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            return null;
+        }
     }
 
     public int getNumberOfWordsInText(String text) {
