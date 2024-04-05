b65677c34b2bb60b3b2288dfae299ed76e93e5f0
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-05 20:20:50 +0200

    add TextVectorsLoader.loadTextVectors()

--- KNN Text Classification/src/main/java/org/example/TextVectorsLoader.java
+++ KNN Text Classification/src/main/java/org/example/TextVectorsLoader.java
@@ -1,2 +1,31 @@
-package org.example;public class TextVectorsLoader {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextVectorsLoader {
+
+    public TextVectorsLoader() {
+    }
+
+    public List<TextVector> loadTextVectors(List<Article> articles) {
+        List<TextVector> textVectors = new ArrayList<>();
+        FeaturesExtractor featuresExtractor = new FeaturesExtractor();
+        for (Article a : articles) {
+            String body = a.getBody();
+            TextVector textVector = new TextVector(a.getCountry(),
+                    featuresExtractor.extractFirstBigLettersSeries(body),
+                    featuresExtractor.extractRelativeNumberOfWordsLongerThan9Signs(body),
+                    featuresExtractor.extractRelativeNumberOfUniqueWords(body),
+                    featuresExtractor.extractMostCommonBigLetterWordInFirstXPercent(body, 0.20),
+                    featuresExtractor.extractMostCommonBigLetterWordInFirstXPercent(body, 0.50),
+                    featuresExtractor.extractMostCommonBigLetterWordInFirstXPercent(body, 1.00),
+                    featuresExtractor.extractRelativeNumberOfNumbers(body),
+                    featuresExtractor.extractMostCommonBigLettersSeriesInText(body),
+                    "CURRENCY_PLACEHOLDER",
+                    "COUNTRY_PLACEHOLDER",
+                    "CONTINENT_PLACEHOLDER");
+        }
+    }
+
 }
