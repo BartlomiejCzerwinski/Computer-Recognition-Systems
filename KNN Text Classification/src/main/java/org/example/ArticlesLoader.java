bdd4e9412b5d77006c38498faa6cb5bca8134d49
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-05 20:08:55 +0200

    add loadArticles()

--- KNN Text Classification/src/main/java/org/example/ArticlesLoader.java
+++ KNN Text Classification/src/main/java/org/example/ArticlesLoader.java
@@ -1,2 +1,32 @@
-package org.example;public class ArticlesLoader {
+package org.example;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ArticlesLoader {
+    public ArticlesLoader() {
+    }
+
+    public List<Article> loadArticles() {
+        ArrayList<Article> articles = new ArrayList<>();
+        File file = new File("reuters21578");
+        ReutersParser reutersParser = new ReutersParser(file);
+        for(ReutersArticle a : reutersParser) {
+            String title = a.getTag("TITLE");
+            String places = a.getTag("PLACES");
+            Pattern pattern = Pattern.compile("<D>(west-germany|usa|france|uk|canada|japan)</D>");
+            Matcher matcher = pattern.matcher(places);
+            while (matcher.find()) {
+                String country = matcher.group(1);
+                String body = a.getTag("BODY");
+                articles.add(new Article(country, title, body));
+                break;
+            }
+        }
+        System.out.println(articles.get(0).getTitle().toString());
+        return articles;
+    }
 }
