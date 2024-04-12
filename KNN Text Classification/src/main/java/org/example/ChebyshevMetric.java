11c76bb1f70eccc927ba1a4ea48c5b2009713512
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-12 16:57:43 +0200

    add chebyshev distance calculation

--- KNN Text Classification/src/main/java/org/example/ChebyshevMetric.java
+++ KNN Text Classification/src/main/java/org/example/ChebyshevMetric.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChebyshevMetric extends Metric{
@@ -9,6 +11,17 @@ public class ChebyshevMetric extends Metric{
 
     @Override
     public double calculateDistance(TextVector vector1, TextVector vector2) {
-        return 0;
+        List<Double> distances = new ArrayList<>();
+        for (int i : featuresList) {
+            if ( i == 1 || i == 2 || i == 6) {
+                distances.add(Math.abs((double) vector1.get(i) - (double) vector2.get(i)));
+            }
+            else {
+                distances.add((double) Math.abs(calculateTextsDistance((String) vector1.get(i), (String) vector2.get(i))));
+            }
+        }
+        distances.sort(Collections.reverseOrder());
+
+        return distances.get(0);
     }
 }
