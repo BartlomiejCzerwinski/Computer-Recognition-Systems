b9336067576a743f2a7783d3d14175766a1c4437
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-12 18:57:38 +0200

    update KNNClassifier

--- KNN Text Classification/src/main/java/org/example/KNNClassifier.java
+++ KNN Text Classification/src/main/java/org/example/KNNClassifier.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import java.net.InterfaceAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 
 public class KNNClassifier {
     private int k;
@@ -13,12 +11,58 @@ public class KNNClassifier {
     private List<TextVector> trainVectors = new ArrayList<>();
     private List<TextVector> testVectors = new ArrayList<>();
 
+
     public KNNClassifier(int k, double divideProportion, List<Integer> featuresList, Metric metric, List<TextVector> textVectors) {
         this.k = k;
         this.divideProportion = divideProportion;
         this.featuresList = featuresList;
         this.metric = metric;
         splitData(textVectors);
+    }
+
+    public void classify() {
+        List<PredictedRealPair> predictedRealPairs = new ArrayList<>();
+        for (TextVector testVector : testVectors) {
+            List<DistanceToCountry> distancesToCountries = new ArrayList<>();
+            for (TextVector trainVector : trainVectors) {
+                distancesToCountries.add(new DistanceToCountry( trainVector.getCountry(), metric.calculateDistance(testVector, trainVector)));
+            }
+            Collections.sort(distancesToCountries);
+            predictedRealPairs.add(new PredictedRealPair(predictCountry(distancesToCountries), testVector.getCountry()));
+        }
+        for (PredictedRealPair predictedRealPair : predictedRealPairs) {
+            System.out.println(predictedRealPair.getPredicted() + " , " + predictedRealPair.getReal());
+        }
+        System.out.println(predictedRealPairs.size());
+    }
+
+    public String predictCountry(List<DistanceToCountry> distancesToCountries) {
+        Map<String, Integer> counters = new HashMap<String, Integer>() {{
+            put("west-germany", 0);
+            put("usa", 0);
+            put("france", 0);
+            put("uk", 0);
+            put("canada", 0);
+            put("japan", 0);
+        }};
+
+        for (int i = 0; i < k; i++) {
+            int currVal = counters.get(distancesToCountries.get(i).getCountry());
+            counters.put(distancesToCountries.get(i).getCountry(), currVal + 1);
+        }
+
+        String countryWithMaxCount = "";
+        int maxCount = 0;
+
+        for (Map.Entry<String, Integer> entry : counters.entrySet()) {
+            String country = entry.getKey();
+            int count = entry.getValue();
+            if (count > maxCount) {
+                maxCount = count;
+                countryWithMaxCount = country;
+            }
+        }
+        return countryWithMaxCount;
     }
 
     public void splitData(List<TextVector> textVectors) {
