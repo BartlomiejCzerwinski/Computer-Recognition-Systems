a0621ea8469ef06914c8a73f56fdcedeb01738fa
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-12 17:27:03 +0200

    add DistanceToCountry

---
+++ KNN Text Classification/src/main/java/org/example/DistanceToCountry.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class DistanceToCountry {
+    private String country;
+    private double distance;
+
+    public DistanceToCountry(String country, double distance) {
+        this.country = country;
+        this.distance = distance;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+}
