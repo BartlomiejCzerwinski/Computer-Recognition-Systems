bc32f4a98d41b78658bc31a643ef2f8f80814b03
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-04-12 17:42:30 +0200

    add Predicted-Real Pair object

---
+++ KNN Text Classification/src/main/java/org/example/PredictedRealPair.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class PredictedRealPair {
+    String predicted;
+    String real;
+
+    public PredictedRealPair(String predicted, String real) {
+        this.predicted = predicted;
+        this.real = real;
+    }
+
+    public String getPredicted() {
+        return predicted;
+    }
+
+    public String getReal() {
+        return real;
+    }
+}
