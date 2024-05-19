b66bacf454ffe24e244c76b9528218c83142b63f
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 22:40:55 +0200

    add Universe of Discourse

--- Linguistic Summarization of Database/summarization/src/main/java/org/example/LinguisticVariable.java
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/LinguisticVariable.java
@@ -7,11 +7,13 @@ public class LinguisticVariable {
     private ArrayList<LinguisticLabel> linguisticLabels;
     private double domainL;
     private double domainR;
+    private UniverseOfDiscourse universeOfDiscourse;
 
-    public LinguisticVariable(String name, ArrayList<LinguisticLabel> linguisticLabels, double domainL, double domainR) {
+    public LinguisticVariable(String name, ArrayList<LinguisticLabel> linguisticLabels, double domainL, double domainR, UniverseOfDiscourse universeOfDiscourse) {
         this.name = name;
         this.linguisticLabels = linguisticLabels;
         this.domainL = domainL;
         this.domainR = domainR;
+        this.universeOfDiscourse = universeOfDiscourse;
     }
 }
