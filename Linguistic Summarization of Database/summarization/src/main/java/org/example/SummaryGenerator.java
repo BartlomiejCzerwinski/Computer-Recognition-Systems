40f1b2835a38e31d1226f15423a2464611e29cca
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 19:40:49 +0200

    init SummaryGenerator

---
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/SummaryGenerator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class SummaryGenerator {
+    private String kind;
+    private int type;
+    // quantifiers
+    // qualifiers
+    private String subject1;
+    private String subject2;
+    private ArrayList<LinguisticVariable> linguisticVariables;
+    //summaries
+}
