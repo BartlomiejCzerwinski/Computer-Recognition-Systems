e4f248e8dfb111b6c08d90e95c027a97cd8c7c5f
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 21:41:35 +0200

    Quantifier update

--- Linguistic Summarization of Database/summarization/src/main/java/org/example/Quantifier.java
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/Quantifier.java
@@ -3,9 +3,11 @@ package org.example;
 public class Quantifier {
     private String name;
     private LinguisticLabel qualifier;
+    private boolean isAbsolute;
 
-    public Quantifier(String name, LinguisticLabel qualifier) {
+    public Quantifier(String name, LinguisticLabel qualifier, boolean isAbsolute) {
         this.name = name;
         this.qualifier = qualifier;
+        this.isAbsolute = isAbsolute;
     }
 }
