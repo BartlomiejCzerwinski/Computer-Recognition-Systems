3d542df035d67bbc0168fe86eed80f932cfe955c
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 19:10:30 +0200

    add Quantifier

---
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/Quantifier.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Quantifier {
+    private String name;
+    private LinguisticLabel linguisticLabel;
+
+    public Quantifier(String name, LinguisticLabel linguisticLabel) {
+        this.name = name;
+        this.linguisticLabel = linguisticLabel;
+    }
+}
