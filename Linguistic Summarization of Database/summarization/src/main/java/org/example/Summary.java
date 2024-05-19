23278aa1ca87031659ea0f25af05e313d0daa1c3
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 19:48:12 +0200

    init Summary

---
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/Summary.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Summary {
+    private String sentence;
+    private String subject1;
+    private String subject2;
+    private int type;
+    private String kind;
+}
