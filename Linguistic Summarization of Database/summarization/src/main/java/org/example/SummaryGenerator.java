2e378f2dbf0521be38c216376309ee76b7dbe926
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 22:16:40 +0200

    update SummaryGenerator

--- Linguistic Summarization of Database/summarization/src/main/java/org/example/SummaryGenerator.java
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/SummaryGenerator.java
@@ -6,9 +6,32 @@ public class SummaryGenerator {
     private String kind;
     private int type;
     private ArrayList<Quantifier> quantifiers;
-    // qualifiers
     private String subject1;
     private String subject2;
     private ArrayList<LinguisticVariable> linguisticVariables;
     private ArrayList<Summary> summaries;
+    private ArrayList<Double> measuresWeights;
+    private ArrayList<String> qualifiers;
+    private ArrayList<String> summarizers;
+
+    public void generateSummaries() {
+        return;
+    }
+
+    public void sortSummaries(int measureID) {
+        return;
+    }
+
+    public SummaryGenerator(String kind, int type, ArrayList<Quantifier> quantifiers, String subject1, String subject2, ArrayList<LinguisticVariable> linguisticVariables, ArrayList<Summary> summaries, ArrayList<Double> measuresWeights, ArrayList<String> qualifiers, ArrayList<String> summarizers) {
+        this.kind = kind;
+        this.type = type;
+        this.quantifiers = quantifiers;
+        this.subject1 = subject1;
+        this.subject2 = subject2;
+        this.linguisticVariables = linguisticVariables;
+        this.summaries = summaries;
+        this.measuresWeights = measuresWeights;
+        this.qualifiers = qualifiers;
+        this.summarizers = summarizers;
+    }
 }
