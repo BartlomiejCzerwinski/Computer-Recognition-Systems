be933a2986c89b4baa58c6fb42418608d512cb9a
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 16:26:25 +0200

    add MembershipFunction properties

--- Linguistic Summarization of Database/summarization/src/main/java/org/example/MembershipFunction.java
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/MembershipFunction.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class MembershipFunction {
+    private double alfaL;
+    private double alfaR;
+    private double domainL;
+    private double domainR;
+
+    public MembershipFunction(double alfaL, double alfaR, double domainL, double domainR) {
+        this.alfaL = alfaL;
+        this.alfaR = alfaR;
+        this.domainL = domainL;
+        this.domainR = domainR;
+    }
 }
