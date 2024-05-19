92633c264438020d6d706256a48f7fd2418bb827
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-19 21:51:53 +0200

    update LinguisticVariable

--- Linguistic Summarization of Database/summarization/src/main/java/org/example/LinguisticVariable.java
+++ Linguistic Summarization of Database/summarization/src/main/java/org/example/LinguisticVariable.java
@@ -5,4 +5,6 @@ import java.util.ArrayList;
 public class LinguisticVariable {
     private String name;
     private ArrayList<LinguisticLabel> linguisticLabels;
+    private double domainL;
+    private double domainR;
 }
