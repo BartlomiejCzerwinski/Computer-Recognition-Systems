b26eb0901e71ac6179c49095d5fa1e1db693cd7b
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-25 12:44:07 +0200

    add Initializer

---
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/Initializer.java
@@ -0,0 +1,4 @@
+package com.example.summarization;
+
+public class Initializer {
+}
--- Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/SummaryGenerator.java
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/SummaryGenerator.java
@@ -34,4 +34,6 @@ public class SummaryGenerator {
         this.qualifiers = qualifiers;
         this.summarizers = summarizers;
     }
+
+
 }
