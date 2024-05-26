3b34d4826d59aeade1f2daf6547935a0e626ff20
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-26 11:48:10 +0200

    update ui

--- Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
@@ -11,13 +11,51 @@ public class HelloController {
 
     @FXML
     private ComboBox<String> comboBoxQuantifier;
+    @FXML
+    private ComboBox<String> comboBoxQualifier;
+    @FXML
+    private ComboBox<String> comboBoxSummarizer;
+    @FXML
+    private ComboBox<String> comboBoxSubject1;
+    @FXML
+    private ComboBox<String> comboBoxSubject2;
+    @FXML
+    private ComboBox<String> comboBoxSummarizerMany;
+    @FXML
+    private ComboBox<String> comboBoxSingleOrMany;
+    @FXML
+    private ComboBox<String> comboBoxType;
+
 
     @FXML
     public void initialize() {
+        Initializer initializer = new Initializer();
+        Quantifier quantifier = initializer.createQuantifier();
+        initializer.createLinguisticVariables();
         comboBoxQuantifier.setItems(FXCollections.observableArrayList(
+                quantifier.getLabelsNames()
+        ));
+        comboBoxQualifier.setItems(FXCollections.observableArrayList(
+                "-"
+        ));
+        comboBoxSummarizer.setItems(FXCollections.observableArrayList(
+                "-"
+        ));
+        comboBoxSubject1.setItems(FXCollections.observableArrayList(
+                "-"
+        ));
+        comboBoxSubject2.setItems(FXCollections.observableArrayList(
+                "-"
+        ));
+        comboBoxSummarizerMany.setItems(FXCollections.observableArrayList(
+                "-"
+        ));
+        comboBoxSingleOrMany.setItems(FXCollections.observableArrayList(
+                "single subject"
+        ));
+        comboBoxType.setItems(FXCollections.observableArrayList(
                 "1",
-                "2",
-                "3"
+                "2"
         ));
     }
 
