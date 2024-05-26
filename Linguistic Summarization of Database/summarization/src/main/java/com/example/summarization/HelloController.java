12e21107c1e85ceffca4bd45e9c72b4874d23340
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-26 10:47:47 +0200

    UI update

--- Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
@@ -1,11 +1,25 @@
 package com.example.summarization;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private ComboBox<String> comboBoxQuantifier;
+
+    @FXML
+    public void initialize() {
+        comboBoxQuantifier.setItems(FXCollections.observableArrayList(
+                "1",
+                "2",
+                "3"
+        ));
+    }
 
     @FXML
     protected void onHelloButtonClick() {
