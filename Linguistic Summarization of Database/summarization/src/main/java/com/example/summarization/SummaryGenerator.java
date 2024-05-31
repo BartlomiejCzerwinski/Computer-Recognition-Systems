4305f1bc4a21dda9223b4a402f7266e8b38b4f2f
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-31 17:37:48 +0200

    summary generator refactor

--- Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/SummaryGenerator.java
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/SummaryGenerator.java
@@ -34,139 +34,77 @@ public class SummaryGenerator {
     }
 
     public void generateSummaries() {
-        if (type == 1) {
-            for (Quantifier quantifier : quantifiers) {
-                int columnIndex = 0;
-                for (LinguisticVariable linguisticVariable : summarizers) {
-                    for (Label label : linguisticVariable.getLabels()) {
-                        double sum = 0.0;
-                        for (Credit credit : credits) {
-                            double value = getValueByColumnIndex(columnIndex, credit);
-                            sum += label.getMembershipFunction().calculateMembershipDegree(value);
-                        }
-                        double totalMembership = sum / credits.size();
-                        for (Label quantifierLabel : quantifier.getLabels()) {
-                            double membershipDegree = quantifierLabel.getMembershipFunction().calculateMembershipDegree(totalMembership);
-                            String summary = quantifierLabel.getName() + " " + subject1 + " are " + label.getName();
-                            System.out.println(summary);
-
-                            calculateAndPrintQualityMeasures(summary, membershipDegree, totalMembership, 0, 0, linguisticVariable.getLabels().size(), summarizers.size(), quantifierLabel.getMembershipFunction().domainR - quantifierLabel.getMembershipFunction().domainL); // Adjust parameters as needed
-                        }
-                    }
-                    columnIndex++;
-                }
-            }
-        } else if (type == 2) {
-            for (Quantifier quantifier : quantifiers) {
-                int columnIndexSummarizer = 0;
-                for (LinguisticVariable summarizer : summarizers) {
-                    for (Label summarizerLabel : summarizer.getLabels()) {
-                        double sumSummarizer = 0.0;
-                        for (Credit credit : credits) {
-                            double valueSummarizer = getValueByColumnIndex(columnIndexSummarizer, credit);
-                            sumSummarizer += summarizerLabel.getMembershipFunction().calculateMembershipDegree(valueSummarizer);
-                        }
-                        double totalMembershipSummarizer = sumSummarizer / credits.size();
-                        for (LinguisticVariable qualifier : summarizers) {
-                            int columnIndexQualifier = 0;
-                            for (Label qualifierLabel : qualifier.getLabels()) {
-                                double sumQualifier = 0.0;
-                                for (Credit credit : credits) {
-                                    double valueQualifier = getValueByColumnIndex(columnIndexQualifier, credit);
-                                    sumQualifier += qualifierLabel.getMembershipFunction().calculateMembershipDegree(valueQualifier);
-                                }
-                                double totalMembershipQualifier = sumQualifier / credits.size();
-                                for (Label quantifierLabel : quantifier.getLabels()) {
-                                    double membershipDegree = quantifierLabel.getMembershipFunction().calculateMembershipDegree(totalMembershipSummarizer);
-                                    String summary = quantifierLabel.getName() + " " + qualifierLabel.getName() + " are " + summarizerLabel.getName();
-
-                                    calculateAndPrintQualityMeasures(summary, membershipDegree, totalMembershipSummarizer, totalMembershipQualifier, credits.size(), summarizer.getLabels().size(), summarizers.size(), quantifierLabel.getMembershipFunction().domainR - quantifierLabel.getMembershipFunction().domainL);
-                                }
-                                columnIndexQualifier++;
-                            }
-                        }
-                        columnIndexSummarizer++;
-                    }
-                }
+        if (kind == "single subject") {
+            switch (type) {
+                case 1:
+                    generateSummariesSingleKindType1();
+                    break;
+                case 2:
+                    generateSummariesSingleKindType2();
+                    break;
             }
         }
-
-        printFirstXSummaries(25);
-    }
-
-    public void printFirstXSummaries(int x) {
-        Collections.sort(summariesWithMeasures);
-        for (int i = 0; i < x; i ++) {
-            System.out.println(summariesWithMeasures.get(summariesWithMeasures.size() - i - 1));
+        else {
+            switch (type) {
+                case 1:
+                    generateSummariesMultipleKindType1();
+                    break;
+                case 2:
+                    generateSummariesMultipleKindType2();
+                    break;
+                case 3:
+                    generateSummariesMultipleKindType3();
+                    break;
+                case 4:
+                    generateSummariesMultipleKindType4();
+                    break;
+            }
         }
     }
 
-    private void calculateAndPrintQualityMeasures(String sentence, double membershipDegreeQuantifier, double totalMembershipSummarizer, double totalMembershipQualifier, int totalCredits, int numberOfLabels, int numberOfSummarizers, double quantifierRange) {
-        double T1 = calculateQualityMeasureT1(membershipDegreeQuantifier, totalCredits);
-        double T2 = calculateQualityMeasureT2(totalMembershipSummarizer);
-        double T3 = calculateQualityMeasureT3(totalMembershipSummarizer);
-        double T4 = calculateQualityMeasureT4(membershipDegreeQuantifier, totalMembershipSummarizer);
-        double T5 = calculateQualityMeasureT5(totalMembershipSummarizer, totalMembershipQualifier);
-        double T6 = calculateQualityMeasureT6(numberOfLabels);
-        double T7 = calculateQualityMeasureT7(numberOfSummarizers);
-        double T8 = calculateQualityMeasureT8(quantifierRange);
-        double T9 = calculateQualityMeasureT9(numberOfSummarizers);
-        double T10 = calculateQualityMeasureT10(quantifierRange);
-        double T11 = calculateQualityMeasureT11(totalMembershipSummarizer);
-        summariesWithMeasures.add(new SimpleSummary(sentence, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11));
-    }
+    public void generateSummariesSingleKindType1() {
+        for (Quantifier quantifier : quantifiers) {
+            int columnIndex = 0;
+            for (LinguisticVariable linguisticVariable : summarizers) {
+                for (Label label : linguisticVariable.getLabels()) {
+                    double sum = 0.0;
+                    for (Credit credit : credits) {
+                        double value = getValueByColumnIndex(columnIndex, credit);
+                        sum += label.getMembershipFunction().calculateMembershipDegree(value);
+                    }
+                    double totalMembership = sum / credits.size();
+                    for (Label quantifierLabel : quantifier.getLabels()) {
+                        double membershipDegree = quantifierLabel.getMembershipFunction().calculateMembershipDegree(totalMembership);
+                        String summary = quantifierLabel.getName() + " " + subject1 + " are " + label.getName();
+                        System.out.println(summary);
 
-
-    private double calculateQualityMeasureT1(double membershipDegree, int totalCredits) {
-        return Math.min(membershipDegree, 1.0);
-    }
-
-    private double calculateQualityMeasureT2(double totalMembershipSummarizer) {
-        return Math.min(totalMembershipSummarizer, 1.0);
-    }
-
-    private double calculateQualityMeasureT3(double totalMembershipSummarizer) {
-        return Math.min(totalMembershipSummarizer, 1.0);
-    }
-
-    private double calculateQualityMeasureT4(double membershipDegreeQuantifier, double totalMembershipSummarizer) {
-        if (totalMembershipSummarizer <= 0.01) {
-            return 0.0;
+                        //calculateAndPrintQualityMeasures(summary, membershipDegree, totalMembership, 0, 0, linguisticVariable.getLabels().size(), summarizers.size(), quantifierLabel.getMembershipFunction().domainR - quantifierLabel.getMembershipFunction().domainL); // Adjust parameters as needed
+                    }
+                }
+                columnIndex++;
+            }
         }
-        return Math.min(membershipDegreeQuantifier / totalMembershipSummarizer, 1.0);
     }
 
-    private double calculateQualityMeasureT5(double totalMembershipSummarizer, double totalMembershipQualifier) {
-        if (totalMembershipQualifier == 0) {
-            return 0.0;
-        }
-        return Math.min(totalMembershipSummarizer / totalMembershipQualifier, 1.0);
+    public void generateSummariesSingleKindType2() {
+
     }
 
-    private double calculateQualityMeasureT6(int numberOfLabels) {
-        return Math.min(1.0 / numberOfLabels, 1.0);
+    public void generateSummariesMultipleKindType1() {
+
     }
 
-    private double calculateQualityMeasureT7(int numberOfSummarizers) {
-        return Math.min(1.0 / numberOfSummarizers, 1.0);
+    public void generateSummariesMultipleKindType2() {
+
     }
 
-    private double calculateQualityMeasureT8(double quantifierRange) {
-        return Math.min(1.0 / quantifierRange, 1.0);
+    public void generateSummariesMultipleKindType3() {
+
     }
 
-    private double calculateQualityMeasureT9(int numberOfQualifiers) {
-        return Math.min(1.0 / numberOfQualifiers, 1.0);
-    }
+    public void generateSummariesMultipleKindType4() {
 
-    private double calculateQualityMeasureT10(double quantifierRange) {
-        return Math.min(1.0 / quantifierRange, 1.0);
     }
-
-    private double calculateQualityMeasureT11(double totalMembershipSummarizer) {
-        return Math.min(totalMembershipSummarizer, 1.0);
-    }
-
 
     public double getValueByColumnIndex(int columnIndex, Credit credit) {
         switch (columnIndex) {
