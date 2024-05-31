5f00a9fa4b25492acedb5441d0e64538f9cae8ac
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-05-31 16:09:12 +0200

    get weights values

--- Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/HelloController.java
@@ -163,7 +163,32 @@ public class HelloController {
 
     @FXML
     public void onConfirmWeightsClick() {
-        System.out.println("Confirm weights clicked!");
+        double t1 = Double.parseDouble(this.t1.getCharacters().toString());
+        double t2 = Double.parseDouble(this.t2.getCharacters().toString());
+        double t3 = Double.parseDouble(this.t3.getCharacters().toString());
+        double t4 = Double.parseDouble(this.t4.getCharacters().toString());
+        double t5 = Double.parseDouble(this.t5.getCharacters().toString());
+        double t6 = Double.parseDouble(this.t6.getCharacters().toString());
+        double t7 = Double.parseDouble(this.t7.getCharacters().toString());
+        double t8 = Double.parseDouble(this.t8.getCharacters().toString());
+        double t9 = Double.parseDouble(this.t9.getCharacters().toString());
+        double t10 = Double.parseDouble(this.t10.getCharacters().toString());
+        double t11 = Double.parseDouble(this.t11.getCharacters().toString());
+
+        if (isWeightsCorrect(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11)) {
+            // save weights
+        }
+        else {
+            // error msg
+        }
+
+    }
+
+    public boolean isWeightsCorrect(double t1, double t2, double t3, double t4, double t5, double t6, double t7,
+                                    double t8, double t9, double t10, double t11) {
+        if ((t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9 + t10 + t11) == 1.0)
+            return true;
+        return false;
     }
 
     @FXML
