dd5a91d4bae73e4f41a2c0f7c2ba0659b8e0c9ee
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-06-02 08:24:36 +0200

    init QualityMeasuresCalculator

---
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/QualityMeasuresCalculator.java
@@ -0,0 +1,11 @@
+package com.example.summarization;
+
+import java.util.ArrayList;
+
+public class QualityMeasuresCalculator {
+    private ArrayList<Credit> credits;
+
+    public QualityMeasuresCalculator(ArrayList<Credit> credits) {
+        this.credits = credits;
+    }
+}
