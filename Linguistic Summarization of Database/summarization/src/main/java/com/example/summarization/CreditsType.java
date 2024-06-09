73f76772923e6bca4c8f3884333a8070c72361e7
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-06-09 15:53:25 +0200

    add CreditsType enum

---
+++ Linguistic Summarization of Database/summarization/src/main/java/com/example/summarization/CreditsType.java
@@ -0,0 +1,11 @@
+package com.example.summarization;
+
+public enum CreditsType {
+    CREDIT_TYPE,
+    CAR,
+    SMALL_BUSINESS,
+    DEBT_CONSOLIDATION,
+    HOME_IMPROVEMENT,
+    MAJOR_PURCHASE,
+    MEDICAL
+}
